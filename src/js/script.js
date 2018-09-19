1bba11355c66fbdaa31b84edd5dfaa547f14f3ad
Yaciner <redjalay@gmail.com>
2018-09-19 18:00:18 +0200

    DOM of home section && loaded bodymovin && updated all modules

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,13 @@
-const init = () => {
+import bodymovin from 'lottie-web';
 
+const init = () => {
+  bodymovin.loadAnimation({
+    container: document.querySelector(`.anim`),
+    renderer: `svg`,
+    loop: true,
+    autoplay: true,
+    path: `../assets/data/data.json`
+  });
 };
 
 init();
